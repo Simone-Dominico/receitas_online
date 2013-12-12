e512050ef300752ccf7a98086db9465a2fd86fad
Simone Dominico <simone.dominico@gmail.com>
2013-12-12 19:44:09 -0200

    Finish

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -3,7 +3,7 @@ class Revenue < ActiveRecord::Base
 	belongs_to :category
 	default_scope -> { order('created_at DESC') }
 	validates :name, presence: true
-	validates :preparation, presence: true, length: { maximum: 500 }
+	validates :preparation, presence: true, length: { maximum: 3000 }
 	validates :ingredients, presence: true
 	validates :user_id, presence: true
 	validates :category_id, presence: true
