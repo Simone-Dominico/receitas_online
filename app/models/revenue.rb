d3dac6ef8cef50860dd38295104bc1425d1824bb
Simone Dominico <simone.dominico@gmail.com>
2013-12-12 19:49:17 -0200

    Finish finish

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -3,8 +3,8 @@ class Revenue < ActiveRecord::Base
 	belongs_to :category
 	default_scope -> { order('created_at DESC') }
 	validates :name, presence: true
-	validates :preparation, presence: true, length: { maximum: 3000 }
-	validates :ingredients, presence: true
+	validates :preparation, presence: true, length: { maximum: 5000 }
+	validates :ingredients, presence: true, length: { maximum: 5000 }
 	validates :user_id, presence: true
 	validates :category_id, presence: true
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "120x90#" }, :default_url=> "/images/:styles/missing.png" 
